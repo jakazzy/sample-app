f7aab0ef83a402c32849d9cb724a8a7f1af2e2b4
jakazzy <jakazzy@gmail.com>
2020-07-05 16:31:02 +0000

    style modal

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -9,4 +9,19 @@ let showModal=true
 </div>
 {/if}
 <style>
+.backdrop{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0,0,0,0.8)
+}
+
+.modal{
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background: white;
+}
 </style>
